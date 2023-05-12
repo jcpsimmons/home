ba18a97814d906287827579cae86475f87215260
Josh Simmons <jsimmons@anduril.com>
2023-05-12 08:23:20 -0700

    info

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Install Nix
+2. Run `nix build '.#homeConfigurations.jsimmons.activationPackage'`
+3. Run `./result/activate`
+4. Should be working now
--- programs.nix
+++ programs.nix
@@ -31,6 +31,9 @@
         code $(fzf)
       }
       source ~/.andurilshellconfig
+      export NVM_DIR="$HOME/.nvm"
+      [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+      [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
     '';
     initExtraFirst = ''
       source ~/.p10k.zsh
