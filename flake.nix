f09918e7ec55c3d24855343c043816dc6e87f4d7
josh <joshcsimmons@icloud.com>
2023-05-17 11:06:43 -0700

    cider updates

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Josh's Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
--- packages.nix
+++ packages.nix
@@ -20,12 +20,16 @@ let
     terminator
     rofi
     polybar
-    feh  
+    feh
     # fonts 
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "Meslo" "JetBrainsMono" ]; })
     google-chrome
+    i3
 
-    #flakes
+    # flakes
+    # cider flake (fixes breaking apple API change)
+    (builtins.getFlake "github:jcpsimmons/cider-flake/e6a60324be29cbff5fe15c6b27cf0f65ecdf7046").packages."${pkgs.system}".default
+    # prsearch cli
     (builtins.getFlake "github:jcpsimmons/prsearch/63618ec2d04e5ae227e10588b221471e391664d1").packages."${pkgs.system}".default
   ];
 in
